f61cca2dbcc299b90b49aa25446aecbfa8ac1470
matthewdias <matthewdias@me.com>
2017-07-04 16:59:15 -0500

    bump version

--- strategies/vrv.js
+++ strategies/vrv.js
@@ -8,4 +8,4 @@ var getEpisode = function() {
   return parseInt(document.querySelector('.title').innerText.split(' ').slice(1, 2)[0])
 }
 
-module.exports = { playerName, getTitle, getEpisode}
+module.exports = { playerName, getTitle, getEpisode }
